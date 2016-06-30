b53ce37c70ceaf3e6b0d39dd971f2dc8a70de3a5
User <oleogin@gmail.com>
2016-06-30 16:33:36 +0300

    http service provider was added

---
+++ src/main/java/general/http/HttpServiceProvider.java
@@ -0,0 +1,11 @@
+package general.http;
+
+import org.apache.http.impl.client.HttpClients;
+
+public class HttpServiceProvider {
+
+    public HttpService get()
+    {
+        return new HttpService(HttpClients.custom().build());
+    }
+}
